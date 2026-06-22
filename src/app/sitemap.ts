99828afbf56ccdd66d5a9e88d4866015adfb1ae0
naokk <naokki.kn@gmail.com>
2026-06-23 01:01:34 +0900

    Fix sitemap BASE_URL to devtools-hub.vercel.app

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 import { tools } from '@/data/tools'
 
-const BASE_URL = 'https://devtools-hub.dev'
+const BASE_URL = 'https://devtools-hub.vercel.app'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const toolUrls: MetadataRoute.Sitemap = tools.map((tool) => ({
